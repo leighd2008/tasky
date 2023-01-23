0117eddcbe86a71307758cd2ad2fc01068a26ad0
Diane Leigh <leighd2008@gmail.com>
2023-01-23 18:08:16 -0500

    fixed the route problem so react app now visible in electron window

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import React, { Component } from "react";
-import { createRoot } from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
 import ReactDOM from "react-dom";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import App from "./components/App";
 
-const container = document.getElementById('app');
-const root = createRoot(container);
+// const container = document.getElementById('app');
+// const root = createRoot(container);
 
-root.render(
+// root.render(
+  ReactDOM.render(
   <Router>
-    <Routes>
-      <Route
-        component={() => (
-          <App />
-        )}
-      />
-    </Routes>
+    {/* <Routes> */}
+      {/* <Route */}
+        {/* component={() => ( */}
+          <App />,
+         {/* )} */}
+      {/* /> */}
+    {/* </Routes> */}
   </Router>,
   document.getElementById("root")
 );
