bd66b89afe8b594ad3efadc7b9435a952f4f36a1
Diane Leigh <leighd2008@gmail.com>
2023-01-23 19:55:37 -0500

    created tray icon and set functionality to open and close main window, haven't tested on ios or windows

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
+const path = require('path');
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Tray, Menu } = electron;
 
 let mainWindow;
+let tray;
+let trayMenu;
+
+process.platform === 'linux' ? trayMenu = Menu.buildFromTemplate([
+    {
+      label: 'Open',
+      click() { mainWindow.show()}
+    },
+    {
+      label: 'Hide',
+      click() { mainWindow.hide()}
+    }
+  ])
+ : null;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
@@ -10,10 +25,29 @@ app.on('ready', () => {
     width: 300,
     frame: false,
     resizable: false,
+    show: false,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
     }
   });
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+  
 });
+
+app.on('ready', () => {
+  const iconName = process.platform === 'darwin' ? 'iconTemplate.png' : 'windows-icon.png'
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+  tray = new Tray(iconPath);
+  process.platform === 'linux' ? tray.setContextMenu(trayMenu) 
+    : tray.on('click', () => {
+      if (mainWindow.isVisible()) {
+        mainWindow.hide();
+      } else {
+        mainWindow.show();
+      }
+    });
+    
+  
+  
+})
