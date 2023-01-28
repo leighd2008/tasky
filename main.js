9173099801736124c680e5253802d433e8562c2e
Diane Leigh <leighd2008@gmail.com>
2023-01-28 16:25:55 -0500

    hide mainWindow when user clicks away

--- main.js
+++ main.js
@@ -2,36 +2,9 @@ const path = require('path');
 const electron = require('electron');
 const TimerTray = require('./app/timer_tray')
 
-const { app, BrowserWindow, Tray, Menu } = electron;
+const { app, BrowserWindow  } = electron;
 
 let mainWindow;
-let tray;
-let trayMenu;
-
-// process.platform === 'linux' ? trayMenu = Menu.buildFromTemplate([
-//     {
-//       label: 'Open',
-//       click(event, bounds) {
-//         //  Click event bounds
-//         const { x, y} = electron.screen.getCursorScreenPoint()
-        
-//         //  Window height and width
-//         const { height, width } = mainWindow.getBounds();
-//         const yPosition = y;
-//         mainWindow.setBounds({
-//           x: x -width / 2,
-//           y: yPosition,
-//           height: height,
-//           width: width,
-//         })
-//         mainWindow.show()}
-//     },
-//     {
-//       label: 'Hide',
-//       click() { mainWindow.hide()}
-//     }
-//   ])
-//  : null;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
@@ -50,6 +23,9 @@ app.on('ready', () => {
     }
   });
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+  mainWindow.on('blur', () => {
+    mainWindow.hide();
+  });
   
 });
 
@@ -57,8 +33,5 @@ app.on('ready', () => {
   const iconName = process.platform === 'darwin' ? 'iconTemplate.png' : 'windows-icon.png'
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
   tray = new TimerTray(iconPath, mainWindow);
-  
-  // process.platform === 'linux' ? tray.setContextMenu(trayMenu) 
-    // : null
   
 })
